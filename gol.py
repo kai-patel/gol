692e1ab2254162ef55caa7c48dfc6c67210e76fd
Kai Patel <00kaipatel@gmail.com>
2022-09-14 17:09:06 +0100

    Formatting with black

--- gol.py
+++ gol.py
@@ -3,15 +3,16 @@ import numpy as np
 from matplotlib import animation
 import random
 
-cells = np.array([[random.randint(0, 1) for _ in range(16)] for _ in range(16)])
+cells = np.array([[random.randint(0, 1) for _ in range(256)] for _ in range(256)])
 
-#cells[6][6] = 1
-#cells[7][6] = 1
-#cells[8][6] = 1
+# cells[6][6] = 1
+# cells[7][6] = 1
+# cells[8][6] = 1
 
 fig = plt.figure()
 ax = plt.axes()
 plot = plt.matshow(cells, fignum=0)
+
 
 def iterate(a):
     print(a)
@@ -28,16 +29,17 @@ def getNeighbours(i, j, cells):
     minH, maxH = 0, len(cells[i]) - 1
     minV, maxV = 0, len(cells) - 1
 
-    for v in range(i-1, i+2):
-        for h in range(j-1, j+2):
+    for v in range(i - 1, i + 2):
+        for h in range(j - 1, j + 2):
             if minH <= h <= maxH and minV <= v <= maxV:
-                    if not (v == i and h == j):
-                        #print(f"Checking neighbour for ({i}, {j}) at ({v}, {h})")
-                        if cells[v][h] == 1:
-                            #print(f"Cell ({v}, {h}) is on and next to ({i}, {j})")
-                            total += 1
-                        
+                if not (v == i and h == j):
+                    # print(f"Checking neighbour for ({i}, {j}) at ({v}, {h})")
+                    if cells[v][h] == 1:
+                        # print(f"Cell ({v}, {h}) is on and next to ({i}, {j})")
+                        total += 1
+
     return total
+
 
 def checkRules(i, j, cells):
     cell = cells[i][j]
@@ -56,5 +58,5 @@ def checkRules(i, j, cells):
 
 
 anim = animation.FuncAnimation(fig, iterate, interval=100, frames=None, blit=True)
-#iterate(1)
+# iterate(1)
 plt.show()
